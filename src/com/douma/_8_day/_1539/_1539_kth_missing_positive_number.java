6957cd77d9356601df62cf2a7e0cd565fa0ebdb8
Jeffy Tang <tangwq_ok@163.com>
2021-08-11 17:01:52 +0800

    补充 1539 注释说明

--- src/com/douma/_8_day/_1539/_1539_kth_missing_positive_number.java
+++ src/com/douma/_8_day/_1539/_1539_kth_missing_positive_number.java
@@ -11,6 +11,15 @@ public class _1539_kth_missing_positive_number {
     // 元素 a[i] 之前缺失的正整数的个数为：a[i] - i - 1
     public int findKthPositive(int[] arr, int k) {
         if (arr[0] > k) return k;
+        /*
+        解释：right 为什么是 arr.length 而非 arr.length - 1
+        因为这里你需要考虑缺失的数字可能是数组最后一个元素的下一个整数，
+        所以，这里进行二分的时候，范围必须是 [0....nums.length] 中查找
+        比如：[1, 2, 3, 4, 5]  k = 1，
+        那么这个时候第 1 个缺失的数字是 6 ，是数组最后一个元素的下一个整数
+
+        就是说你要查找的数字有可能超出数组的范围
+         */
         int left = 0, right = arr.length;
         while (left < right) {
             int mid = left + (right - left) / 2;
