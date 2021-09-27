e04dddba7d5375e30ef0202bfe6f7637b861f32b
Jeffy Tang <tangwq_ok@163.com>
2021-09-27 14:15:30 +0800

    腾讯笔试题代码

---
+++ src/com/douma/written_test/meituan/_1_Cake_Split.py
@@ -0,0 +1,32 @@
+# 抖码算法，让算法学习变得简单有趣
+# 作者：老汤
+
+if __name__ == '__main__':
+    # 第一行是切的刀数
+    n = int(input())
+
+    # longitudes 记录经度的度数是否被切(经度作为数组的索引)，就是竖着切
+    # latitudes 记录纬度的度数是否被切(纬度作为数组的索引)，就是横着切
+    longitudes, latitudes = [False] * 365, [False] * 365
+
+    # 分别记录竖着切和横着切的次数
+    lng_cnt, lat_cnt = 0, 0
+
+    # 接下来是 n 刀切的情况
+    for i in range(n):
+        data = str(input()).split(" ")
+        if data[0] == "1":
+            lng = int(data[1])
+            if not longitudes[lng]:
+                lng_cnt += 1
+                longitudes[lng] = True
+        elif data[0] == "0":
+            lat = int(data[1])
+            if not latitudes[lat]:
+                lat_cnt += 1
+                latitudes[lat] = True
+
+    if lng_cnt == 0:
+        print(str(lat_cnt + 1))
+    else:
+        print(str((lng_cnt * 2) * (lat_cnt + 1)))
---
+++ src/com/douma/written_test/tengxun/_1_20210926.py
@@ -0,0 +1,61 @@
+# 抖码算法，让算法学习变得简单有趣
+# 微信公众号：抖码课堂
+# 作者：老汤
+
+
+# 时间复杂度是：O(logn)，因为树的高度平均为 logn，n 的大小最大为 10^5
+def is_ancestor(i, j, parents):
+    while j != 1:
+        if i == j:
+            return True
+        j = parents[j]
+    return False
+
+
+# 详见 leetcode 367. 有效的完全平方数：https://leetcode-cn.com/problems/valid-perfect-square/
+# 时间复杂度是：O(logn)，因为这道题目的节点的值最大为 100，所以这里的 num 最大为 100 * 100 = 10000
+# log10000 约等于 13 ，这个性能是可以接受的
+def is_square_number(num):
+    if num < 2:
+        return True
+
+    left, right = 2, num // 2
+
+    while left <= right:
+        x = left + (right - left) // 2
+        guess_squared = x * x
+        if guess_squared == num:
+            return True
+        if guess_squared > num:
+            right = x - 1
+        else:
+            left = x + 1
+
+    return False
+
+
+# 暴力解法
+# 时间复杂度：O(n^2 * logn)
+# 空间复杂度：O(1)
+if __name__ == '__main__':
+    # 第一行是节点的个数
+    n = int(input())
+
+    data, parents = [0] * (n + 1), [0] * (n + 1)
+    # 第二行是每个节点的值
+    nodes = str(input()).split(" ")
+    for i in range(n):
+        data[i + 1] = int(nodes[i])
+    # 第三行是每个节点的父亲节点的索引
+    nodeParents = str(input()).split(" ")
+    parents[1] = 1
+    for i in range(n - 1):
+        parents[i + 2] = int(nodeParents[i])
+
+    res = 0
+    for i in range(1, n + 1):
+        for j in range(i + 1, n + 1):
+            if is_ancestor(i, j, parents) and is_square_number(data[i] * data[j]):
+                res += 1
+
+    print(res)
