6e04f766aeac5dabeb3971447cbd7a68bb8a59a6
Jeffy Tang <tangwq_ok@163.com>
2021-09-30 12:25:06 +0800

    26 题支持 C 语言

---
+++ src/com/douma/_1_first_day/_26/代码实现_C.c
@@ -0,0 +1,15 @@
+int removeDuplicates(int* nums, int numsSize){
+    if (numsSize == 0) {
+        return 0;
+    }
+    int slow = 0;
+    int fast = 1;
+    while (fast < numsSize) {
+        if (nums[fast] != nums[slow]) {
+            slow++;
+            nums[slow] = nums[fast];
+        }
+        fast++;
+    }
+    return slow + 1;
+}
