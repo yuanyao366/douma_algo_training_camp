29bd653c740c62d4342b2e1bce0248deecbfc2a6
Jeffy Tang <tangwq_ok@163.com>
2021-10-25 14:45:46 +0800

    leetcode 459 注释

--- src/com/douma/_4_day/_459/_459_repeated_substring_pattern.java
+++ src/com/douma/_4_day/_459/_459_repeated_substring_pattern.java
@@ -11,6 +11,14 @@ public class _459_repeated_substring_pattern {
 
     // 字符串匹配法
     public boolean repeatedSubstringPattern(String s) {
+        /*
+        字符串 s 拼接的目的：可以在 s + s 中找到 s 的所有旋转后的字符串
+        拼接后的字符串的头部是旋转了一圈的字符串，而尾部是没有旋转的字符串，所以需要去掉头部和尾部
+
+        从 1 开始匹配的作用就是去掉头部
+
+        不等于 s.length 的作用就是去掉尾部
+         */
         return (s + s).indexOf(s, 1) != s.length();
     }
 
--- src/com/douma/_4_day/_459/代码实现_Python3.py
+++ src/com/douma/_4_day/_459/代码实现_Python3.py
@@ -1,4 +1,12 @@
 def repeatedSubstringPattern(self, s: str) -> bool:
+    """
+    字符串 s 拼接的目的：可以在 s + s 中找到 s 的所有旋转后的字符串
+    拼接后的字符串的头部是旋转了一圈的字符串，而尾部是没有旋转的字符串，所以需要去掉头部和尾部
+
+    从 1 开始匹配的作用就是去掉头部
+
+    不等于 s.length 的作用就是去掉尾部
+    """
     return (s + s).find(s, 1) != len(s)
 
 
