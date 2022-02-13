67ac43f328d7e48d3f40f4541d4173c8353de242
Jeffy Tang <tangwq_ok@163.com>
2022-02-13 12:28:11 +0800

    416 另一种方案【使用计算组合数】说明

--- src/com/douma/_27_day/_416/代码实现_JavaScript.js
+++ src/com/douma/_27_day/_416/代码实现_JavaScript.js
@@ -12,6 +12,14 @@ var canPartition = function(nums) {
     const target = Math.floor(sum / 2)
 
     // dp[c] : 表示从 nums 中是否可以找到总和等于 c 的元素组合
+    /*
+    说明：这里不就是数组 nums 中不可重复的选择数字组合，是否存在和等于 target 的组合呢？
+        我们可以不可以和前面讲过的目标和题目一样，这样定义状态呢：
+            dp[c] : 表示从 nums 中找到总和等于 c 的元素组合数
+        状态转移方程为：dp[c] = dp[c] + dp[c - nums[i]];
+        最后的话，我们只要判断 dp[target] 是否大于 0 即可
+    注意：这种方案是不行的，详情请参考 issue：https://gitee.com/douma_edu/douma_algo_training_camp/issues/I4TGJ2
+     */
     const dp = new Array(target + 1).fill(false)
     dp[0] = true
 
--- src/com/douma/_27_day/_416/代码实现_Python3.py
+++ src/com/douma/_27_day/_416/代码实现_Python3.py
@@ -10,6 +10,14 @@ class Solution:
         target = sum(nums) // 2
 
         # dp[c] : 表示从 nums 中是否可以找到总和等于 c 的元素组合
+        '''
+        说明：这里不就是数组 nums 中不可重复的选择数字组合，是否存在和等于 target 的组合呢？
+            我们可以不可以和前面讲过的目标和题目一样，这样定义状态呢：
+                dp[c] : 表示从 nums 中找到总和等于 c 的元素组合数
+            状态转移方程为：dp[c] = dp[c] + dp[c - nums[i]];
+            最后的话，我们只要判断 dp[target] 是否大于 0 即可
+        注意：这种方案是不行的，详情请参考 issue：https://gitee.com/douma_edu/douma_algo_training_camp/issues/I4TGJ2
+        '''
         dp = [False] * (target + 1)
         dp[0] = True
 
