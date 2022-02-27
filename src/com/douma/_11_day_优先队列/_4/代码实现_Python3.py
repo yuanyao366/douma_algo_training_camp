6272ba1bff88c0f5a7979a6f8ee7560554fb12d5
Jeffy Tang <tangwq_ok@163.com>
2022-02-27 20:55:14 +0800

    4 二分解法的 tips

--- src/com/douma/_11_day_优先队列/_4/代码实现_Python3.py
+++ src/com/douma/_11_day_优先队列/_4/代码实现_Python3.py
@@ -15,9 +15,11 @@ class Solution:
             if i == len1: return nums2[j + k - 1]
             if j == len2: return nums1[i + k - 1]
             if k == 1: return min(nums1[i], nums2[j])
-
+            # tips：计算 newi 和 newj 需要减 1 的原因：k/2 表示的是长度，长度是从 1 开始，而下标是从 0 开始的，所以需要减 1
             newi = min(i + k // 2, len1) - 1
             newj = min(j + k // 2, len2) - 1
+            # tips：而下面计算 i 和 j 的时候加 1 的原因是：就是排除 i 前面或者 j 前面的元素，所以往前走一个
+            # 计算 k 的时候加 1 的原因是：用从 0 开始的下标计算从 1 开始的长度，都需要加 1 的
             if nums1[newi] < nums2[newj]:
                 k = k - (newi - i + 1)
                 i = newi + 1
